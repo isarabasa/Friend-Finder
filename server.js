9446a6681b9b2cff934e9ef35e9670382fdaa4d2
Isabela Rabasa <isabelarabasa@Isabelas-MacBook-Pro.local>
2019-06-12 11:25:14 -0500

    ready server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require("express");
+var app = express();
+var path = require("path");
+
+var PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+var mysql = require("mysql");
+
+// require the HTML routes
+require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
+
+app.listen(PORT, function() {
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
